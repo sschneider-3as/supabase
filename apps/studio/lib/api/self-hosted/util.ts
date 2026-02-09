e9008356b0302424feb3a985f352d6f203450a14
Simon Schneider <simon.schneider2@adesso-service.com>
2026-02-09 12:27:51 +0000

    feat(studio): add POSTGRES_SSL_MODE env variable
    To work with external PostgreSQL servers that use SSL, add the
    POSTGRES_SSL_MODE variable for use in studio

--- apps/studio/lib/api/self-hosted/util.ts
+++ apps/studio/lib/api/self-hosted/util.ts
@@ -6,6 +6,7 @@ import {
   POSTGRES_HOST,
   POSTGRES_PASSWORD,
   POSTGRES_PORT,
+  POSTGRES_SSL_MODE,
   POSTGRES_USER_READ_ONLY,
   POSTGRES_USER_READ_WRITE,
 } from './constants'
@@ -26,6 +27,7 @@ export function encryptString(stringToEncrypt: string): string {
 
 export function getConnectionString({ readOnly }: { readOnly: boolean }) {
   const postgresUser = readOnly ? POSTGRES_USER_READ_ONLY : POSTGRES_USER_READ_WRITE
+  const postgresOptions = POSTGRES_SSL_MODE != '' ? `?sslmode=${POSTGRES_SSL_MODE}` : ''
 
-  return `postgresql://${postgresUser}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`
+  return `postgresql://${postgresUser}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}${postgresOptions}`
 }
